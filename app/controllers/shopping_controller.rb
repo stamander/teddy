f7f600f8efe13707818d6e52fa99440d9b921dfe
stamander <iida.atsushi.fh@gmail.com>
2020-07-21 14:23:54 +0900

    Update shopping_controller.rb

--- app/controllers/shopping_controller.rb
+++ app/controllers/shopping_controller.rb
@@ -3,9 +3,16 @@ class ShoppingController < ApplicationController
   end
 
   def create
+    @shopping = Shopping.new(shop_params)
+    if @shopping.sava
+      redirect_to shopping_index_path
+    else
+      redirect_to new_shopping_path
+    end
+    
   end
 
-  
+
 
   def new
     @shopping = Shopping.new
